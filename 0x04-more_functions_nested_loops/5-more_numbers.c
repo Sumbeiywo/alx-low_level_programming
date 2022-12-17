49c27319e9738023b757afdd9b14257b4bf91fd9
Sumbeiywo <briankibett711@gmail.com>
2022-12-17 10:09:41 -0800

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,21 @@
 
 /**
  * more_numbers - nmbr fct
- * BRIAN file
+ * Sumbeiywo file
  * Return: no return
  */
 
 void more_numbers(void)
 {
 	int i, j;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
-			putchar(j % 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
